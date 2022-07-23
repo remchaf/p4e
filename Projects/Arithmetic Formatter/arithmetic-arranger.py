3de942c8355ab843c062f0d879d7b527a760dcaa
remchaf <70279203+remchaf@users.noreply.github.com>
2022-07-23 11:52:54 +0000

    Just added arithmetic formatter directory to git

---
+++ Projects/Arithmetic Formatter/arithmetic-arranger.py
@@ -0,0 +1,10 @@
+def arithmetic_arranger(problems, answer = True):
+    if type(problems) is not 'list' : return 'Wrong input'
+    elif len(problems) > 5 : return 'Error: Too many problems.'
+
+    arranged_problems = list()
+    
+    for op in problems :
+        pass
+
+    return arranged_problems
